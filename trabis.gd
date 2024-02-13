0def56f5a964ce058aa905d56e8fbedb15b85d9e
Travis Nygard <NygardTravi01@isd882.k12.mn.us>
2024-02-13 14:33:52 -0600

    die
    die

--- trabis.gd
+++ trabis.gd
@@ -8,22 +8,24 @@ const JUMP_HORIZONTAL = 600
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = 980
 
-
 func _physics_process(delta):
-	# Add the gravity.
+
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
-	# Handle jump.
+	# Jump Func
 	if Input.is_action_just_pressed("up") and is_on_floor():
 		velocity += Vector2(JUMP_HORIZONTAL,JUMP_VELOCITY)
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	# 
 	var direction = Input.get_axis("left", "right")
 	if direction:
 		velocity.x = lerp(velocity.x, direction * SPEED, .3)
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+	
+	#character fl;ip
+	if get_global_mouse_position().x >= position.x:
+		$Sprite2D
 
-	move_and_slide()
+
--- weapon.gd
+++ weapon.gd
@@ -9,14 +9,13 @@ signal clicked
 func _physics_process(delta):
 	mouse_position = get_global_mouse_position()
 	
-	rotation = lerp_angle(rotation, atan2(mouse_position.y - global_position.y, mouse_position.x - global_position.x), 20 * delta)
+	rotation = atan2(mouse_position.y - global_position.y, mouse_position.x - global_position.x)
 	
 	
 	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT) and press:
 		emit_signal('clicked', Vector2(cos(rotation), sin(rotation)) * 200, 0)
 		press = false
-		for i in 10:
-			move_local_x(2.0)
+		for i in 10:			move_local_x(2.0)
 		#global_position = global_position.move_toward(mouse_position, 22)
 		for a in get_overlapping_bodies():
 			if a.has_method('hurt') and a != get_parent():
